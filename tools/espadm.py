3444f24067d446044b12f3eea1489be1343b82aa
Masakazu Iwanaga <ccldaout@gmail.com>
2018-03-04 20:01:44 +0900

    refactored

--- tools/espadm.py
+++ tools/espadm.py
@@ -13,51 +13,74 @@ if len(sys.argv) == 1:
     print '       espadm reset'
     exit()
 
-def put_small(args):
-    for path in args:
+def show_result(f):
+    def _f(self, path):
+        ret = f(self, path)
+        print path, '...', ret
+        return ret
+    return _f
+
+class AdminCommand(object):
+
+    def __init__(self):
+        self.cli = None
+        self.on_close = None
+
+    def start(self, ip_address):
+        self.cli = ipc.SimpleClient((ip_address, 2000), ipc.JSONPacker())
+        self.cli.start()
+
+    def stop(self):
+        self.cli.close()
+        if self.on_close:
+            self.on_cloase()
+
+    def _send(self, msg):
+        self.cli.send(msg)
+        return self.cli.recv()[0] == 'success'
+
+    @show_result
+    def put_small(self, path):
         with open(path, 'rb') as f:
             data = binascii.hexlify(f.read())
-            cli.send(['put', path, data])
-            m = cli.recv()
-            print path, '...', m[0]
+            return self._send(['put', path, data])
 
-def put(args):
-    for path in args:
+    @show_result
+    def put(self, path):
         with open(path, 'rb') as f:
-            cli.send(['put_beg', path])
-            if cli.recv()[0] == 'failure':
-                continue
-
+            if not self._send(['put_beg', path]):
+                return False
             while True:
                 data = f.read(2048)
                 if not data:
                     break
                 data = binascii.hexlify(data)
-                cli.send(['put_data', data])
-                if cli.recv()[0] == 'failure':
-                    continue
-                
-            cli.send(['put_end', data])
-            m = cli.recv()
-            print path, '...', m[0]
+                if not self._send(['put_data', data]):
+                    return False
+            return self._send(['put_end', data])
 
-def mkdir(args):
-    for path in args:
-        cli.send(['mkdir', path])
-        m = cli.recv()
-        print path, '...', m[0]
+    @show_result
+    def mkdir(self, path):
+        return self._send(['mkdir', path])
 
-cli = ipc.SimpleClient((IP_ADDRESS, 2000), ipc.JSONPacker())
-cli.start()
+    def reset(self):
+        self.cli.send(['reset'])
+        self.stop()
+
+admin = AdminCommand()
+admin.start(IP_ADDRESS)
 
 if sys.argv[1] == 'reset':
-    cli.send(['reset'])
+    admin.reset()
 
 elif sys.argv[1] == 'put':
-    put(sys.argv[2:])
+    for path in sys.argv[2:]:
+        admin.put(path)
 
 elif sys.argv[1] == 'putsmall':
-    put_small(sys.argv[2:])
+    for path in sys.argv[2:]:
+        admin.put_small(path)
 
 elif sys.argv[1] == 'mkdir':
-    mkdir(sys.argv[2:])
+    for path in sys.argv[2:]:
+        admin.mkdir(path)
