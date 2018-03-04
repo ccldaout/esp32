573fef4fe044a1942ad0e3d80acd821f2c39f798
Masakazu Iwanaga <ccldaout@gmail.com>
2018-03-04 10:19:51 +0000

    convert tab to spaces

--- pythonista/mdialog.py
+++ pythonista/mdialog.py
@@ -3,19 +3,19 @@
 import ui
 
 class MessageDialog(object):
-	def __init__(self):
-		self.view = ui.load_view('mdialog')
-		self.textview = self.view['message_view']
-		self.ok_button = self.view['ok_button']
-		self.ok_button.action = self.close
-		
-	def open(self):
-		self.textview.text = ''
-		self.view.present('popover')
-	
-	def close(self, sender=None):
-		self.view.close()
+    def __init__(self):
+        self.view = ui.load_view('mdialog')
+        self.textview = self.view['message_view']
+        self.ok_button = self.view['ok_button']
+        self.ok_button.action = self.close
 
-	def put(self, message):
-		self.textview.text += message
+    def open(self):
+        self.textview.text = ''
+        self.view.present('popover')
+
+    def close(self, sender=None):
+        self.view.close()
+
+    def put(self, message):
+        self.textview.text += message
 
--- pythonista/utils.py
+++ pythonista/utils.py
@@ -3,29 +3,30 @@
 import cPickle
 
 class Config(object):
-	@staticmethod
-	def load(path):
-		try:
-			with open(path) as f:
-				config = cPickle.load(f)
-		except:
-			config = Config()
-		config._path = path
-		return config
+    @staticmethod
+    def load(path):
+        try:
+            with open(path) as f:
+                config = cPickle.load(f)
+        except:
+            config = Config()
+        config._path = path
+        return config
 
-	def __getattr__(self, name):
-		if name[:2] == '__':
-			raise AttributeError()
-		return None
+    def __getattr__(self, name):
+        if name[:2] == '__':
+            raise AttributeError()
+        return None
 
-	def get(self, name, default=None):
-		if hasattr(self, name):
-			return getattr(self, name)
-		return default
-		
-	def save(self):
-		try:
-			with open(self._path, 'wb') as f:
-				cPickle.dump(self, f, cPickle.HIGHEST_PROTOCOL)
-		except:
-			raise
+    def get(self, name, default=None):
+        if hasattr(self, name):
+            return getattr(self, name)
+        return default
+
+    def save(self):
+        try:
+            with open(self._path, 'wb') as f:
+                cPickle.dump(self, f, cPickle.HIGHEST_PROTOCOL)
+        except:
+            raise
+
