5d4ef0e0d8a0255bf993d0f524864b2ccebf0a03
Masakazu Iwanaga <ccldaout@gmail.com>
2018-03-30 10:04:16 +0900

    vsrfsd: change command syntax

--- tools/vsrfsd.py
+++ tools/vsrfsd.py
@@ -4,6 +4,7 @@
 import functools
 import os
 import stat
+import sys
 import time
 from tpp import mipc
 from tpp import toolbox as tb
@@ -39,16 +40,16 @@ class VSRfsService(mipc.ServiceBase):
         else:
             return os.path.abspath(self._root + os.path.sep + in_path)
 
-    @debug
     @mipc.autoreply
+    @debug
     def check_chdir(self, in_path):
         path = self._fullpath(in_path)
         if os.path.isdir(path):
             return path[self._root_len:]
         raise Exception('%s is not directory.' % in_path)
 
-    @debug
     @mipc.autoreply
+    @debug
     def ilistdir(self, in_path):
         oh = self._oh()
         path = self._fullpath(in_path)
@@ -57,8 +58,8 @@ class VSRfsService(mipc.ServiceBase):
             self._objs[oh] = (path, it)
         return oh
 
-    @debug
     @mipc.autoreply
+    @debug
     def ilistdir_next(self, oh):
         with self._obj_lock:
             path, it = self._objs[oh]
@@ -79,33 +80,33 @@ class VSRfsService(mipc.ServiceBase):
             mode = 0
         return (f, mode, s.st_ino, s.st_size)
 
-    @debug
     @mipc.autoreply
+    @debug
     def mkdir(self, in_path):
         path = self._fullpath(in_path)
         os.mkdir(path)
 
-    @debug
     @mipc.autoreply
+    @debug
     def remove(self, in_path):
         path = self._fullpath(in_path)
         os.remove(path)
 
-    @debug
     @mipc.autoreply
+    @debug
     def rename(self, in_path1, in_path2):
         path1 = self._fullpath(in_path1)
         path2 = self._fullpath(in_path2)
         os.rename(path1, path2)
 
-    @debug
     @mipc.autoreply
+    @debug
     def rmdir(self, in_path):
         path = self._fullpath(in_path)
         os.rmdir(path)
 
-    @debug
     @mipc.autoreply
+    @debug
     def stat(self, in_path):
         path = self._fullpath(in_path)
         try:
@@ -121,16 +122,16 @@ class VSRfsService(mipc.ServiceBase):
         return (mode, s.st_ino, s.st_dev, s.st_nlink, s.st_uid, s.st_gid,
                 s.st_size, s.st_atime, s.st_mtime, s.st_ctime)
 
-    @debug
     @mipc.autoreply
+    @debug
     def statvfs(self, in_path):
         path = self._fullpath(in_path)
         s = os.statvfs(path)
         return (s.f_bsize, s.f_frsize, s.f_blocks, s.f_bfree, s.f_bavail,
                 s.f_files, s.f_ffree, s.f_favail, s.f_flag, s.f_namemax)
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_open(self, in_path, mode='r'):
         path = self._fullpath(in_path)
         fobj = open(path, mode)
@@ -139,8 +140,8 @@ class VSRfsService(mipc.ServiceBase):
             self._objs[oh] = fobj
         return (oh, path[self._root_len:])
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_read(self, oh, pos, size):
         with self._obj_lock:
             if oh not in self._objs:
@@ -151,8 +152,8 @@ class VSRfsService(mipc.ServiceBase):
         pos = f.tell()
         return (pos, data)
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_readline(self, oh, pos, size):
         with self._obj_lock:
             if oh not in self._objs:
@@ -163,8 +164,8 @@ class VSRfsService(mipc.ServiceBase):
         pos = f.tell()
         return (pos, data)
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_write(self, oh, pos, data):
         with self._obj_lock:
             if oh not in self._objs:
@@ -175,8 +176,8 @@ class VSRfsService(mipc.ServiceBase):
         pos = f.tell()
         return (pos, n)
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_seek(self, oh, offset, whence=os.SEEK_SET):
         with self._obj_lock:
             if oh not in self._objs:
@@ -186,8 +187,8 @@ class VSRfsService(mipc.ServiceBase):
         pos = f.tell()
         return pos
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_flush(self, oh):
         with self._obj_lock:
             if oh not in self._objs:
@@ -197,8 +198,8 @@ class VSRfsService(mipc.ServiceBase):
         pos = f.tell()
         return pos
 
-    @debug
     @mipc.autoreply
+    @debug
     def f_close(self, oh):
         with self._obj_lock:
             if oh not in self._objs:
@@ -207,6 +208,18 @@ class VSRfsService(mipc.ServiceBase):
             del self._objs[oh]
             f.close()
 
-port = mipc.udp_server(2002)
-mipc.manager.register(port, VSRfsService('~/prog/esp32/export_vsrfs'))
-time.sleep(-1)
+def main(addr, root_dir, foreground=True):
+    port = mipc.udp_server(addr)
+    mipc.manager.register(port, VSRfsService(root_dir))
+    if foreground:
+        time.sleep(-1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'Usage: vsrfsd [host]:port root_dir'
+        exit(1)
+    host, port = sys.argv[1].split(':')
+    root_dir = sys.argv[2]
+    if not os.path.isdir(root_dir):
+        raise TypeError('%s is not directory.' % root_dir)
+    main((host, int(port)), root_dir)
