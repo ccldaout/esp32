b1dcf35babe01067d3859ebbd29a931700ec51e1
Masakazu Iwanaga <ccldaout@gmail.com>
2018-07-09 15:51:23 +0900

    testing

--- upy/test/pulse02.py
+++ upy/test/pulse02.py
@@ -11,17 +11,15 @@ class PeakFinder(object):
         self.__raw_n = raw_n
         self.depth = depth
         self.callback = lambda _:None
-        self.__count = 0
         self.__sum = 0
         self.__dv = [0] * (h_width * 2 + 1)
         self.__rv = [0] * raw_n
         self.__sup_val = 0
-        self.__sup_idx = 0
+        self.__sup_tms = 0
         self.__inf_val = 0
-        self.__inf_idx = 0
+        self.__inf_tms = 0
 
-    def update(self, new_val):
-        self.__count += 1
+    def update(self, new_tms, new_val):
         self.__sum -= self.__rv.pop(0)
         self.__rv.append(new_val)
         self.__sum += new_val
@@ -32,26 +30,26 @@ class PeakFinder(object):
         mid = dv[hn]
         oth = dv[:hn] + dv[hn+1:]
         if mid > max(oth):
-            if (self.__sup_idx and self.__inf_idx and
+            if (self.__sup_tms and self.__inf_tms and
                 (mid - self.__inf_val) > self.depth):
-                self.callback(self.__count - self.__sup_idx)
-            self.__sup_val, self.__sup_idx = mid, self.__count
-            self.__inf_val, self.__inf_idx = 0, 0
+                self.callback(new_tms - self.__sup_tms)
+            self.__sup_val, self.__sup_tms = mid, new_tms
+            self.__inf_val, self.__inf_tms = 0, 0
         elif mid < min(oth):
-            self.__inf_val, self.__inf_idx = mid, self.__count
+            self.__inf_val, self.__inf_tms = mid, new_tms
 
 adc = machine.ADC(machine.Pin(35))
 
 def meas(itv_ms=25):
     _disp.clear()
-    def found(n):
-        hc = 60000.0/(n * itv_ms)
+    def found(tms):
+        hc = 60000.0/tms
         hc_s = '%5.1f' % hc
         _disp.clear()
         _disp.gcf_put(5, 5, hc_s)
     pf = PeakFinder(int(300/itv_ms), 5)
     pf.callback = found
     while True:
-        tv_next_ms = ticks_ms() + itv_ms
-        pf.update(adc.read())
-        sleep_ms(tv_next_ms - ticks_ms())
+        v = adc.read()
+        pf.update(ticks_ms(), v)
+        sleep_ms(itv_ms)
