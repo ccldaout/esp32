3759003aec46eaaee61507d3c9266d775189aa39
Masakazu Iwanaga <ccldaout@gmail.com>
2020-04-29 20:17:21 +0900

    editing max32664

--- upy/lib/max32664.py
+++ upy/lib/max32664.py
@@ -1,6 +1,22 @@
 import machine
 import time
 
+
+class BPM(object):
+    def __init__(self, ba):
+        self.heart_rate = ((ba[0] << 8)|ba[1]) * 0.1
+        self.confidence = ba[2]
+        self.SpO2 = ((ba[3] << 8)|ba[4]) * 0.1
+        self.status = ba[5]
+
+    def __repr__(self):
+        return "BPM(heart_rate:%f, confidence:%d%%, SpO2:%f%%, status:%d)" % (
+            self.heart_rate, self.confidence, self.SpO2, self.status
+        )
+
+    __str__ = __repr__
+
+
 class MAX32664(object):
 
     __addr = 0x55
@@ -10,8 +26,9 @@ class MAX32664(object):
     def __init__(self, scl=22, sda=21, rst=32, mfio=33):
         self.__i2c = machine.I2C(scl=machine.Pin(scl),
                                  sda=machine.Pin(sda))
-        self.__mfio = machine.Pin(mfio, machine.Pin.OUT, 1)
+        self.__mfio = machine.Pin(mfio, machine.Pin.OUT)
         self.__rst = machine.Pin(rst, machine.Pin.OUT)
+        self.__mfio.value(1)
         self.__rst.value(1)
         self.__rst.value(0)
         time.sleep_ms(10)
@@ -20,71 +37,94 @@ class MAX32664(object):
         self.__mfio.init(machine.Pin.IN, machine.Pin.PULL_UP)
 
     def init(self):
-        self.__cmd('\x02\x00')
-        b = self.__read_byte()
+        self._cmd([0x02, 0x00])
+        b = self._readstatus()
         if b != 0:
             raise Exception('Unexpceted Device Mode: %d' % b)
 
         # Set Output Mode
-        self.__cmd('\x10\x00\x02')
-        b = self.__read_byte()
-        print('Set Output Mode:', b)
+        self._cmd([0x10, 0x00, 0x02])
+        s = self._readstatus()
+        print('Set Output Mode:', s)
 
         # Set FIFO Threshold
-        self.__cmd('\x10\x01\x01')
-        b = self.__read_byte()
-        print('Set FIFO Threshold:', b)
+        self._cmd([0x10, 0x01, 0x01])
+        s = self._readstatus()
+        print('Set FIFO Threshold:', s)
 
         # Enable AGC algorithm
-        self.__enable('\x52\x00\x01')
-        b = self.__read_byte()
-        print('Enable AGC algorithm:', b)
+        self._enable([0x52, 0x00, 0x01])
+        s = self._readstatus()
+        print('Enable AGC algorithm:', s)
 
         # Enable MAX30101 sensor
-        self.__enable('\x44\x03\x01')
-        b = self.__read_byte()
-        print('Enable MAX30101 sensor:', b)
+        self._enable([0x44, 0x03, 0x01])
+        s = self._readstatus()
+        print('Enable MAX30101 sensor:', s)
 
 	# Enable MaximFast algorithm
-        self.__enable('\x52\x02\x01')
-        b = self.__read_byte()
-        print('Enable MaximFast algorithm:', b)
+        self._enable([0x52, 0x02, 0x01])
+        s = self._readstatus()
+        print('Enable MaximFast algorithm:', s)
 
         # Read # of samples to avgerage
-        self.__cmd('\x51\x00\x03')
-        b = self.__read_byte()
-        print('Read # of samples to avgerage', b)
+        self._cmd([0x51, 0x00, 0x03])
+        s, n = self._readreply(1)
+        print('Read # of samples to avgerage', s, n)
 
         time.sleep(1)
 
-    def __cmd(self, bs):
-        self.__i2c.writeto(self.__addr, bs, True)
+    def _cmd(self, bs):
+        self.__i2c.writeto(self.__addr, bytes(bs), True)
         time.sleep_ms(self.__delay_cmd_ms)
 
-    def __enable(self, bs):
-        self.__i2c.writeto(self.__addr, bs, True)
+    def _enable(self, bs):
+        self.__i2c.writeto(self.__addr, bytes(bs), True)
         time.sleep_ms(self.__delay_enable_ms)
 
-    def __read_byte(self):
+    def _readstatus(self):
         bs = self.__i2c.readfrom(self.__addr, 1, True)
         return int(bs[0])
 
-    def read(self):
+    def _readreply(self, n=1):
+        res = self.__i2c.readfrom(self.__addr, n+1, True)
+        if n == 1:
+            return res[0], res[1]
+        else:
+            return res[0], res[1:]
+
+    def get(self):
         # Read sensor hub status
-        self.__cmd('\x00\x00')
-        b = self.__read_byte()
-        if b != 0:
-            raise Exception('Unexpceted Sensor HUB status: %d' % b)
+        notify = False
+        while True:
+            self._cmd([0x00, 0x00])
+            s, r = self._readreply()
+            if s != 0:
+                raise Exception('Unexpected read status: %d' % s)
+            if (r & 0x1) != 0:
+                raise Exception('Unexpceted Sensor HUB status: %d' % r)
+            if (r & 0x8) != 0:
+                break
+            if not notify:
+                notify = True
+                print('NO DataRdyInt ... 0x%x' % r)
+            time.sleep_ms(10)
 
         # Read # of samples available in the FIFO
-        self.__cmd('\x12\x00')
-        b = self.__read_byte()
-        print('Read # of samples available in the FIFO', b)
+        self._cmd([0x12, 0x00])
+        s, r = self._readreply()
+        print('Read # of samples available in the FIFO', r)
 
         # Read data stored in output FIFO
-        self.__cmd('\x12\x01')
-        bs = self.__i2c.readfrom(self.__addr, 6, True)
-        return bs
+        self._cmd([0x12, 0x01])
+        time.sleep_ms(10)
+        s, rv = self._readreply(6)
+        return BPM(rv)
+
+    @property
+    def mfio(self):
+        return self.__mfio()
+
 
 mx = MAX32664()
 mx.init()
